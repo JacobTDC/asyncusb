dcb92a7493f03b2e894c4dbbcae258c9b8190365
JacobTDC <14812197+JacobTDC@users.noreply.github.com>
2026-01-09 13:46:50 -0600

    rebase: cleaned _types.py
    - cleaned imports
    - removed unused and commented out code

--- src/asyncusb/_types.py
+++ src/asyncusb/_types.py
@@ -1,10 +1,4 @@
-import asyncio
-import collections.abc
 from ctypes import Structure
-from functools import wraps
-from inspect import signature
-from types import GenericAlias
-from weakref import WeakKeyDictionary, ref
 
 
 
@@ -12,13 +6,10 @@ def _struct_proxy_property(name, mutable=True):
     def getter(self):
         return getattr(self._contents, name)
 
-    if mutable:
-        def setter(self, value):
-            setattr(self._contents, name, value)
-    else:
-        setter = None
+    def setter(self, value):
+        setattr(self._contents, name, value)
 
-    return property(getter, setter)
+    return property(getter, setter if mutable else None)
 
 class _StructProxy:
     __slots__ = ('_contents')
@@ -68,59 +59,3 @@ class ImmutableStructProxyMeta(type):
 
 class ImmutableStructProxy(_StructProxy, metaclass=ImmutableStructProxyMeta):
     __slots__ = ()
-
-
-
-#class ScopedSingletonMeta(type):
-#    def __init__(self, name, bases, attrs):
-#        self.__instance_dict = WeakKeyDictionary()
-#
-#        # Restore the signature.
-#        sig = signature(self.__init__)
-#        parameters = tuple(sig.parameters.values())
-#        self.__signature__ = sig.replace(parameters=parameters[1:])
-#
-#        return super().__init__(name, bases, attrs)
-#
-#    def __call__(self, scope, /, *args, **kwargs):
-#        if scope in self.__instance_dict:
-#            instance_ref = self.__instance_dict[scope]
-#            if instance_ref is None:
-#                raise RuntimeError("invalid scope")
-#            elif (instance := instance_ref()) is not None:
-#                return instance
-#
-#        instance = super().__call__(scope, *args, **kwargs)
-#        self.__instance_dict[scope] = ref(instance)
-#        return instance
-#
-#    def _invalidate_scope(self, scope):
-#        self.__instance_dict[scope] = None
-#
-#    def _get_instance(self, scope):
-#        instance_ref = self.__instance_dict.get(scope)
-#        if instance_ref is not None:
-#            return instance_ref()
-#
-#class ScopedSingleton(metaclass=ScopedSingletonMeta):
-#    __slots__ = ('__weakref__', '_scope')
-#
-#    def __init__(self, scope):
-#        self._scope = scope
-#
-#    @classmethod
-#    def _invalidate_scope(cls, scope):
-#        instance = cls._get_instance(scope)
-#        if instance is not None:
-#            instance._scope = None
-#        type(cls)._invalidate_scope(cls, scope)
-#
-#    @staticmethod
-#    def _scope_valid(method):
-#        """A method decorator that raises an error if the scope is invalid."""
-#        @wraps(method)
-#        def wrapper(self, /, *args, **kwargs):
-#            if self._scope is None:
-#                raise RuntimeError("invalid scope")
-#            return method(self, *args, **kwargs)
-#        return wrapper
