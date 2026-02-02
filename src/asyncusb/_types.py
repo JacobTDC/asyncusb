603b9d232ca7e9b04bc776df7d34d2d124507d46
JacobTDC <14812197+JacobTDC@users.noreply.github.com>
2026-02-02 17:40:29 -0600

    refactor: optimized util.find
    The find function is now iterative instead of recursive.

--- src/asyncusb/_types.py
+++ src/asyncusb/_types.py
@@ -1,12 +1,36 @@
-class FindFilter:
-    def __init__(self, typ, query):
-        self._type = typ
+from functools import reduce
+from typing import Any, Callable
 
-        if isinstance(query, tuple):
-            self._query, *self._extras = query
+
+def _noop(_) -> bool:
+    return True
+
+
+class FindFilter:
+    def __init__(self, typ: type, query: dict[str, Any] | None = None,
+                 *subfilters: 'FindFilter'):
+        self._type = typ
+        self._query = query
+        self._subfilters = subfilters
+        self._test: Callable[[object], bool]
+
+        if query:
+            attrs = tuple(zip(map(lambda name: name.split('.'), query.keys()),
+                              query.values()))
+
+            def func(obj) -> bool:
+                try:
+                    for name, val in attrs:
+                        if reduce(getattr, name, obj) != val:
+                            return False
+                except AttributeError:
+                    return False
+
+                return True
+
+            self._test = func
         else:
-            self._query = query
-            self._extras = None
+            self._test = _noop
 
     def __repr__(self):
         if self._type.__module__ == 'builtins':
@@ -14,15 +38,17 @@ class FindFilter:
         else:
             name = f"{self._type.__module__}.{self._type.__name__}"
 
-        if self._extras:
-            args = ", ".join([repr(self._query), *map(repr, self._extras)])
-            return f"{name}[{args}]"
-        else:
-            return f"{name}[{repr(self._query)}]"
+        args = ", ".join((repr(self._query), *map(repr, self._subfilters)))
+        return f"{name}[{args}]"
+
 
 class FilterableMeta(type):
     def __getitem__(self, key):
-        return FindFilter(self, key)
+        if isinstance(key, tuple):
+            return FindFilter(self, *key)
+        else:
+            return FindFilter(self, key)
+
 
 class Filterable(metaclass=FilterableMeta):
     """
@@ -30,5 +56,7 @@ class Filterable(metaclass=FilterableMeta):
     """
     __slots__ = ()
 
-def getfilterparts(ffilter: FindFilter) -> tuple[type, dict, list]:
-    return ffilter._type, ffilter._query or {}, ffilter._extras or []
+
+def getfilterparts(ffilter: FindFilter) -> tuple[type, Callable[[object], bool],
+                                                 tuple[FindFilter, ...]]:
+    return ffilter._type, ffilter._test, ffilter._subfilters
--- src/asyncusb/util.py
+++ src/asyncusb/util.py
@@ -11,64 +11,91 @@ def flatten(iterable: Iterable, depth: int = 1) -> Iterator:
     return iter(iterable)
 
 
+# This function is not public, and may change without notice.
+# Operates in O(V + E) time..
 def _type_dfs(iterable, typ):
     """
-    A generator function that flattens an iterable tree, stopping at
-    instances of the specified type. Essentially a depth-first search for
-    the given type.
+    A generator function that flattens an iterable tree, yielding instances
+    of the specified type. Essentially a depth-first search for the given
+    type.
     """
 
-    current_iter = iter(iterable)
     parent_id = id(iterable)
-
-    stack = []
+    stack = [(iter(iterable), parent_id)]
     visited = {parent_id}
 
-    while True:
-        try:
-            while True:
-                item = next(current_iter)
+    while stack:
+        current_iter, parent_id = stack.pop()
 
-                if isinstance(item, typ):
-                    yield item
+        while (item := next(current_iter, _null)) is not _null:
+            if isinstance(item, typ):
+                yield item
 
-                elif (isinstance(item, Iterable) and
-                      not isinstance(item, (str, bytes))):
-                    item_id = id(item)
-                    if item_id in visited:
-                        continue
+            elif (isinstance(item, Iterable) and
+                  not isinstance(item, (str, bytes))):
+                item_id = id(item)
+                if item_id in visited:
+                    continue
 
-                    stack.append((current_iter, parent_id))
-                    visited.add(item_id)
-                    current_iter = iter(item)
-                    parent_id = item_id
+                stack.append((current_iter, parent_id))
+                visited.add(item_id)
+                current_iter = iter(item)
+                parent_id = item_id
 
-        except StopIteration:
-            visited.remove(parent_id)
-            if not stack:
-                break
-            current_iter, parent_id = stack.pop()
+        visited.remove(parent_id)
 
 
-# A sentinel used to ensure that unset attributes never match.
+# A sentinel value.
 _null = object()
 
-# TODO: find a way to remove recursion from subfilter checking
-def find(obj: Iterable, ffilter: FindFilter) -> Iterator:
+# This is a fully iterative implementation, and the only limit to how deep
+# it can search is how much memory is available.
+# Operates in O(D × (V + E)) time (worst case).
+def find(iterable: Iterable, ffilter: FindFilter) -> Iterator:
     """
     Takes an iterable and lazily deep-searches it for a match to the given
     filter.
     """
 
-    ftype, query, extras = getfilterparts(ffilter)
-    items = _type_dfs(obj, ftype)
+    ftype, query, filters = getfilterparts(ffilter)
+    stack = [(iterable, _type_dfs(iterable, ftype), query, filters, 0)]
 
-    return filter(lambda item:
-        (all(getattr(item, attr, _null) == query[attr] for attr in query) and
-         all(any(find(item, extra)) for extra in extras)), items)
+    while stack:
+        _, current_iter, query, filters, idx = stack.pop()
+
+        while (item := next(current_iter, _null)) is not _null:
+            if not query(item):
+                # Query doesn't match, so skip this item.
+                continue
+
+            while True:
+                if idx < len(filters):
+                    # We still have subfilters to check for this item.
+                    if isinstance(item, Iterable):
+                        # Item is iterable, so save current state to stack
+                        # and begin matching children against subfilters.
+                        stack.append((item, current_iter, query, filters, idx))
+                        ftype, query, filters = getfilterparts(filters[idx])
+                        current_iter = _type_dfs(item, ftype)
+                        idx = 0
+
+                elif stack:
+                    # Unwind the stack one level.
+                    item, current_iter, query, filters, idx = stack.pop()
+                    idx += 1
+                    continue
+
+                else:
+                    # We've reached the bottom of the stack, so this item
+                    # is a match.
+                    yield item
+
+                # Get the next item from the current iterator.
+                break
 
 
 __all__ = [
+    "FindFilter",
     "flatten",
     "find"
 ]
